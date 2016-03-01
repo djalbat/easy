98903bc1bbae4b5412d517d5d084184f1544afad
djalbat <james.smith@djalbat.com>
2016-03-01 13:16:42 +0000

    Added a change handler to the Input element.

--- docs/examples.js
+++ docs/examples.js
@@ -3,14 +3,19 @@
 define(function(require) {
 
   var easyui = require('./../dist/easyui'),
+      Element = easyui.Element,
       Link = easyui.Link,
+      Input = easyui.Input,
       Button = easyui.Button,
       Select = easyui.Select,
-      Checkbox = easyui.Checkbox,
-      Element = easyui.Element;
+      Checkbox = easyui.Checkbox;
 
   var link = new Link('#link', function(href) {
-    console.log('Link click with href ' + href);
+    console.log('Link click with href: ' + href);
+  });
+
+  var input = new Input('#input', function(value) {
+    console.log('Input changed with value: ' + value);
   });
 
   var button = new Button('#button', function() {
@@ -18,11 +23,11 @@ define(function(require) {
   });
 
   var select = new Select('#select', function(selectedOptionValue) {
-    console.log('Select changed with selected option value ' + selectedOptionValue)
+    console.log('Select changed with selected option value: ' + selectedOptionValue)
   });
 
   var checkbox = new Checkbox('#checkbox', function(checked) {
-    console.log('Checkbox click with checked ' + checked);
+    console.log('Checkbox click with checked: ' + checked);
   });
 
   checkbox.check();
@@ -33,7 +38,7 @@ define(function(require) {
       br4 = br3.clone();
 
   var clonedLink = Link.clone('#link', function(href) {
-    console.log('Cloned link click with href ' + href);
+    console.log('Cloned link click with href: ' + href);
   });
 
   clonedLink.removeAttribute('id');
@@ -48,13 +53,13 @@ define(function(require) {
   clonedButton.html('Cloned button');
 
   var selectFromHTML = Select.fromHTML('<select><option>a</option><option>b</option><option>c</option></select>', function(selectedOptionValue) {
-    console.log('Select from HTML changed with selected option value ' + selectedOptionValue)
+    console.log('Select from HTML changed with selected option value: ' + selectedOptionValue)
   });
 
   selectFromHTML.setSelectedOptionByValue('c');
 
   var checkboxFromHTML = Checkbox.fromHTML('<input type="checkbox"/>', function(checked) {
-    console.log('Checkbox from HTML click with checked ' + checked);
+    console.log('Checkbox from HTML click with checked: ' + checked);
   });
 
   checkbox.appendAfter(br1);
