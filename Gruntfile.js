797d5a7c77f26a84e8ef704ffd666b06202a8e22
James Smith <james.smith@djalbat.com>
2016-04-08 14:14:03 +0100

    Again minor changes to the Grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: './libES2015/',
-          src: ['*.js'],
+          src: ['**/*.js'],
           dest: './lib/'
         }]
       }
