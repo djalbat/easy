9e6584da54a3557bf31daecbe4a59c4b924ed801
djalbat <james.smith@djalbat.com>
2017-03-13 12:17:44 +0000

    Improved the applyProperties() method.

--- es6/mixin/jsx.js
+++ es6/mixin/jsx.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function applyProperties(properties) {
+  this.properties = {};
+
   const names = Object.keys(properties);
 
   names.forEach(function(name) {
@@ -13,13 +15,14 @@ function applyProperties(properties) {
     } else {
       const value = properties[name];
 
-      if (isHandlerName(name)) {
-        const eventType = eventTypeFromName(name),
-              handler = value;  ///
+      if (false) {
 
-        this.on(eventType, handler);
+      } else if (isHandlerName(name)) {
+        addHandler(this, name, value);
+      } else if (isAttributeName(name)) {
+        addAttribute(this, name, value);
       } else {
-        applyPropertyAsAttribute(this, name, value);
+        this.properties[name] = value;
       }
     }
   }.bind(this));
@@ -31,7 +34,14 @@ const jsxMixin = {
 
 module.exports = jsxMixin;
 
-function applyPropertyAsAttribute(element, name, value) {
+function addHandler(element, name, value) {
+  const eventType = eventTypeFromName(name),
+        handler = value;  ///
+
+  element.on(eventType, handler);
+}
+
+function addAttribute(element, name, value) {
   if (name === 'className') {
     name = 'class';
   }
@@ -61,6 +71,34 @@ function isHandlerName(name) {
   return name.match(/^on/);
 }
 
+function isAttributeName(name) {
+  return attributeNames.includes(name);
+}
+
 function eventTypeFromName(name) {
   return name.substr(2).toLowerCase();
 }
+
+const attributeNames = [
+  'accept', 'acceptCharset', 'accessKey', 'action', 'allowFullScreen', 'allowTransparency', 'alt', 'async', 'autoComplete', 'autoFocus', 'autoPlay',
+  'capture', 'cellPadding', 'cellSpacing', 'challenge', 'charSet', 'checked', 'cite', 'classID', 'className', 'colSpan', 'cols', 'content', 'contentEditable', 'contextMenu', 'controls', 'coords', 'crossOrigin',
+  'data', 'dateTime', 'default', 'defer', 'dir', 'disabled', 'download', 'draggable',
+  'encType',
+  'form', 'formAction', 'formEncType', 'formMethod', 'formNoValidate', 'formTarget', 'frameBorder',
+  'headers', 'height', 'hidden', 'high', 'href', 'hrefLang', 'htmlFor', 'httpEquiv',
+  'icon', 'id', 'inputMode', 'integrity', 'is',
+  'keyParams', 'keyType', 'kind',
+  'label', 'lang', 'list', 'loop', 'low',
+  'manifest', 'marginHeight', 'marginWidth', 'max', 'maxLength', 'media', 'mediaGroup', 'method', 'min', 'minLength', 'multiple', 'muted',
+  'name', 'noValidate', 'nonce',
+  'open', 'optimum',
+  'pattern', 'placeholder', 'poster', 'preload', 'profile',
+  'radioGroup', 'readOnly', 'rel', 'required', 'reversed', 'role', 'rowSpan', 'rows',
+  'sandbox', 'scope', 'scoped', 'scrolling', 'seamless', 'selected', 'shape', 'size', 'sizes', 'span', 'spellCheck', 'src', 'srcDoc', 'srcLang', 'srcSet', 'start', 'step', 'style', 'summary',
+  'tabIndex', 'target', 'title', 'type',
+  'useMap',
+  'value',
+  'width',
+  'wmode',
+  'wrap'
+];
