bb5b156635e739ff19abba24064bdfe983118457
James Smith <james.smith@djalbat.com>
2022-01-27 11:35:14 +0000

    Tiny, cosmetic changes.

--- src/utilities/array.js
+++ src/utilities/array.js
@@ -4,6 +4,20 @@ export function first(array) { return array[0]; }
 
 export function push(array1, array2) { Array.prototype.push.apply(array1, array2); }
 
+export function augment(array1, array2, test) {
+  array1 = [ ...array1 ]; ///
+
+  array2.forEach((element, index) => {
+    const passed = test(element, index);
+
+    if (passed) {
+      array1.push(element);
+    }
+  });
+
+  return array1;
+}
+
 export function flatten(array) {
   return array.reduce((array, element) => {
     array = array.concat(element);  ///
@@ -18,18 +32,4 @@ export function guarantee(arrayOrElement) {
   return (arrayOrElement instanceof Array) ?
             arrayOrElement :
               [arrayOrElement];
-}
-
-export function augment(array1, array2, test) {
-  array1 = [ ...array1 ]; ///
-
-  array2.forEach((element, index) => {
-    const passed = test(element, index);
-
-    if (passed) {
-      array1.push(element);
-    }
-  });
-
-  return array1;
 }
