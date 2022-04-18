cfc4ab445f959d91c249df1e67007cf47e69b09f
James Smith <james.smith@djalbat.com>
2022-04-18 18:07:21 +0100

    Added double click mixins.

--- src/mixins/click.js
+++ src/mixins/click.js
@@ -1,14 +1,20 @@
 "use strict";
 
-import { CLICK_EVENT_TYPE } from "../eventTypes";
+import { CLICK_EVENT_TYPE, DBLCLICK_EVENT_TYPE } from "../eventTypes";
 
 function onClick(clickHandler, element) { this.on(CLICK_EVENT_TYPE, clickHandler, element); }
 
 function offClick(clickHandler, element) { this.off(CLICK_EVENT_TYPE, clickHandler, element); }
 
+function onDoubleClick(clickHandler, element) { this.on(DBLCLICK_EVENT_TYPE, clickHandler, element); }
+
+function offDoubleClick(clickHandler, element) { this.off(DBLCLICK_EVENT_TYPE, clickHandler, element); }
+
 const clickMixins = {
   onClick,
-  offClick
+  offClick,
+  onDoubleClick,
+  offDoubleClick
 };
 
 export default clickMixins;
