ef6c4796bd937f81dbd7b5847f27479ce88ba8ac
James Smith <james.smith@djalbat.com>
2022-11-24 12:39:55 +0000

    Removed some of the event types.

--- src/eventTypes.js
+++ src/eventTypes.js
@@ -1,59 +1,41 @@
 "use strict";
 
-export const OPEN_EVENT_TYPE = "open";
-export const MOVE_EVENT_TYPE = "move";
 export const BLUR_EVENT_TYPE = "blur";
-export const DRAG_EVENT_TYPE = "drag";
-export const DROP_EVENT_TYPE = "drop";
 export const WHEEL_EVENT_TYPE = "wheel";
 export const INPUT_EVENT_TYPE = "input";
 export const FOCUS_EVENT_TYPE = "focus";
 export const CLICK_EVENT_TYPE = "click";
 export const KEYUP_EVENT_TYPE = "keyup";
-export const REMOVE_EVENT_TYPE = "remove";
 export const CHANGE_EVENT_TYPE = "change";
 export const RESIZE_EVENT_TYPE = "resize";
 export const SCROLL_EVENT_TYPE = "scroll";
 export const KEYDOWN_EVENT_TYPE = "keydown";
 export const MOUSEUP_EVENT_TYPE = "mouseup";
 export const AUXCLICK_EVENT_TYPE = "auxclick";
-export const DRAG_OUT_EVENT_TYPE = "dragout";
 export const MOUSEOUT_EVENT_TYPE = "mouseout";
 export const DBLCLICK_EVENT_TYPE = "dblclick";
 export const MOUSEOVER_EVENT_TYPE = "mouseover";
 export const MOUSEDOWN_EVENT_TYPE = "mousedown";
 export const MOUSEMOVE_EVENT_TYPE = "mousemove";
-export const DRAG_OVER_EVENT_TYPE = "dragover";
-export const STOP_DRAG_EVENT_TYPE = "stopdrag";
-export const START_DRAG_EVENT_TYPE = "startdrag";
 export const CONTEXTMENU_EVENT_TYPE = "contextmenu";
 
 export default {
-  OPEN_EVENT_TYPE,
-  MOVE_EVENT_TYPE,
   BLUR_EVENT_TYPE,
-  DRAG_EVENT_TYPE,
-  DROP_EVENT_TYPE,
   WHEEL_EVENT_TYPE,
   INPUT_EVENT_TYPE,
   FOCUS_EVENT_TYPE,
   CLICK_EVENT_TYPE,
   KEYUP_EVENT_TYPE,
-  REMOVE_EVENT_TYPE,
   CHANGE_EVENT_TYPE,
   RESIZE_EVENT_TYPE,
   SCROLL_EVENT_TYPE,
   KEYDOWN_EVENT_TYPE,
   MOUSEUP_EVENT_TYPE,
   AUXCLICK_EVENT_TYPE,
-  DRAG_OUT_EVENT_TYPE,
   MOUSEOUT_EVENT_TYPE,
   DBLCLICK_EVENT_TYPE,
   MOUSEOVER_EVENT_TYPE,
   MOUSEDOWN_EVENT_TYPE,
   MOUSEMOVE_EVENT_TYPE,
-  DRAG_OVER_EVENT_TYPE,
-  STOP_DRAG_EVENT_TYPE,
-  START_DRAG_EVENT_TYPE,
   CONTEXTMENU_EVENT_TYPE
 };
