3e1fddfda08bb8ff9351426e5d82826a176545d7
James Smith <james.smith@djalbat.com>
2024-10-12 17:34:49 +0100

    Moved the selection change funtionality from the selection mixins to the document.

--- src/document.js
+++ src/document.js
@@ -7,6 +7,7 @@ import clickMixins from "./mixins/click";
 import selectionMixins from "./mixins/selection";
 
 import { UNDEFINED } from "./constants";
+import { SELECTIONCHANGE_EVENT_TYPE } from "./eventTypes";
 
 class Document {
   constructor() {
@@ -20,6 +21,40 @@ class Document {
   createRange() { return this.domElement.createRange(); }
 
   createTextNode(data) { return this.domElement.createTextNode(data); }
+
+  onSelectionChange(selectionChangeHandler, element) { this.onEvent(SELECTIONCHANGE_EVENT_TYPE, selectionChangeHandler, element); }
+
+  offSelectionChange(selectionChangeHandler, element) { this.offEvent(SELECTIONCHANGE_EVENT_TYPE, selectionChangeHandler, element); }
+
+  createEventListener(eventType, handler, element) {
+    let eventListener;
+
+    const handlerElement = element; ///
+
+    eventListener = (event) => {
+      if (eventType === SELECTIONCHANGE_EVENT_TYPE) {
+        const { domElement } = handlerElement,
+              { currentTarget } = event,
+              { activeElement } = currentTarget;
+
+        if (domElement !== activeElement) {
+          return;
+        }
+      }
+
+      const element = this; ///
+
+      handler.call(handlerElement, event, element);
+    };
+
+    Object.assign(eventListener, {
+      element,
+      handler,
+      eventType
+    });
+
+    return eventListener;
+  }
 }
 
 export default (typeof document === UNDEFINED) ? undefined : new Document();  ///
--- src/mixins/selection.js
+++ src/mixins/selection.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { CUT_EVENT_TYPE, COPY_EVENT_TYPE, PASTE_EVENT_TYPE, SELECTIONCHANGE_EVENT_TYPE } from "../eventTypes";
+import { CUT_EVENT_TYPE, COPY_EVENT_TYPE, PASTE_EVENT_TYPE } from "../eventTypes";
 
 function onCut(cutHandler, element) { this.onEvent(CUT_EVENT_TYPE, cutHandler, element); }
 
@@ -13,40 +13,6 @@ function offCopy(copyHandler, element) { this.offEvent(COPY_EVENT_TYPE, copyHand
 function onPaste(pasteHandler, element) { this.onEvent(PASTE_EVENT_TYPE, pasteHandler, element); }
 
 function offPaste(pasteHandler, element) { this.offEvent(PASTE_EVENT_TYPE, pasteHandler, element); }
-
-function onSelectionChange(selectionChangeHandler, element) { this.onEvent(SELECTIONCHANGE_EVENT_TYPE, selectionChangeHandler, element); }
-
-function offSelectionChange(selectionChangeHandler, element) { this.offEvent(SELECTIONCHANGE_EVENT_TYPE, selectionChangeHandler, element); }
-
-function createEventListener(eventType, handler, element) {
-  let eventListener;
-
-  const handlerElement = element; ///
-
-  eventListener = (event) => {
-    if (eventType === SELECTIONCHANGE_EVENT_TYPE) {
-      const { domElement } = handlerElement,
-            { currentTarget } = event,
-            { activeElement } = currentTarget;
-
-      if (domElement !== activeElement) {
-        return;
-      }
-    }
-
-    const element = this; ///
-
-    handler.call(handlerElement, event, element);
-  };
-
-  Object.assign(eventListener, {
-    element,
-    handler,
-    eventType
-  });
-
-  return eventListener;
-}
 
 function getSelectionStart() { return this.domElement.selectionStart; }
 
@@ -65,9 +31,6 @@ const selectionMixins = {
   offCopy,
   onPaste,
   offPaste,
-  onSelectionChange,
-  offSelectionChange,
-  createEventListener,
   getSelectionStart,
   getSelectionEnd,
   setSelectionStart,
