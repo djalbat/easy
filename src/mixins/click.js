9f18339385a20da014c08f53d7df6715fc8d3481
James Smith <james.smith@djalbat.com>
2024-10-12 19:26:17 +0100

    Added some auxilliary click handlers to the click mixins.

--- src/mixins/click.js
+++ src/mixins/click.js
@@ -1,18 +1,24 @@
 "use strict";
 
-import { CLICK_EVENT_TYPE, DBLCLICK_EVENT_TYPE } from "../eventTypes";
+import { CLICK_EVENT_TYPE, DBLCLICK_EVENT_TYPE, AUXCLICK_EVENT_TYPE } from "../eventTypes";
 
 function onClick(clickHandler, element) { this.onEvent(CLICK_EVENT_TYPE, clickHandler, element); }
 
 function offClick(clickHandler, element) { this.offEvent(CLICK_EVENT_TYPE, clickHandler, element); }
 
-function onDoubleClick(clickHandler, element) { this.onEvent(DBLCLICK_EVENT_TYPE, clickHandler, element); }
+function onAuxClick(auxClickHandler, element) { this.onEvent(AUXCLICK_EVENT_TYPE, auxClickHandler, element); }
 
-function offDoubleClick(clickHandler, element) { this.offEvent(DBLCLICK_EVENT_TYPE, clickHandler, element); }
+function offAuxClick(auxClickHandler, element) { this.offEvent(AUXCLICK_EVENT_TYPE, auxClickHandler, element); }
+
+function onDoubleClick(doubleClickHandler, element) { this.onEvent(DBLCLICK_EVENT_TYPE, doubleClickHandler, element); }
+
+function offDoubleClick(doubleClickHandler, element) { this.offEvent(DBLCLICK_EVENT_TYPE, doubleClickHandler, element); }
 
 const clickMixins = {
   onClick,
   offClick,
+  onAuxClick,
+  offAuxClick,
   onDoubleClick,
   offDoubleClick
 };
